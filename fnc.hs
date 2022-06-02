44cabf7bb9826dd520ca5812cf6d0035d1575659
Daniel Araya Sambucci <danielarayasambucci@estudiantec.cr>
2022-06-02 15:27:39 -0600

    FNC lista y probada
    Realizada en base del código SML generado para la FND.

--- fnc.hs
+++ fnc.hs
@@ -1,71 +1,60 @@
-module Fnc where
+module FNC where
 
 import Sintax
+import AsVals
+import Vars
+import GenBools
+import EvalProp
 
---IMPLICACIÓN Y DISYUNCIÓN
-imd (Implicacion prop1 prop2) = Disyuncion (Negacion prop1) prop2
-
---CASO INVERSO
-imd (Disyuncion (Negacion prop1) prop2) = Implicacion prop1 prop2
---CONTRAPOSITIVA
-contraPositiva (Implicacion prop1 prop2) = Implicacion (Negacion prop2) (Negacion prop1)
---CASO INVERSO
-contraPositiva (Implicacion (Negacion prop1) (Negacion prop2)) = Implicacion prop1 prop2
-
---DOBLE NEGACIÓN
-dn (Negacion (Negacion prop1)) = prop1
-
---DE MORGAN
-dm (Negacion (Disyuncion prop1 prop2)) = Conjuncion (Negacion prop1) (Negacion prop2) 
-dm (Negacion (Conjuncion prop1 prop2)) = Disyuncion (Negacion prop1) (Negacion prop2)
---CASO INVERSO
-dm (Conjuncion (Negacion prop1) (Negacion prop2)) = Negacion (Disyuncion prop1 prop2) 
-dm (Disyuncion (Negacion prop1) (Negacion prop2)) = Negacion (Conjuncion prop1 prop2)
-
---CONMUTATIVA
-con (Disyuncion prop1 prop2) = Disyuncion prop2 prop1
-con (Conjuncion prop1 prop2) = Conjuncion prop2 prop1
---CASO INVERSO
+--Coloca las variables con sus respectivos valores de verdad
+impFnc [] _ _             = ")"
+impFnc ((v,b) : vbs) largo i =  if i < largo then (if b then "~" else "") ++ v ++ " || " ++ impFnc  vbs largo (i+1)
+    else (if b then "~" else "") ++ v ++ impFnc  vbs largo (i+1) 
 
 
---ASOCIATIVA
-aso (Disyuncion (Disyuncion prop1 prop2) prop3) = Disyuncion  prop1 (Disyuncion prop2 prop3)
-aso (Conjuncion (Conjuncion prop1 prop2) prop3) = Conjuncion  prop1 (Conjuncion prop2 prop3)
---CASO INVERSO
+--Llama a impFnd pasándole los parámetros que requiere
+unificarFNC _ [] _ = ""
+unificarFNC vars bools cantV = "(" ++ impFnc (as_vals vars bools) cantV 1
 
 
---DISTRIBUTIVA
-dis (Disyuncion prop1 (Conjuncion prop2 prop3)) = Conjuncion (Disyuncion prop1 prop2) (Disyuncion prop1 prop3)
-dis (Conjuncion prop1 (Disyuncion prop2 prop3)) = Disyuncion (Conjuncion prop1 prop2) (Conjuncion prop1 prop3)
---CASO INVERSO
-dis (Conjuncion (Disyuncion prop1 prop2) (Disyuncion prop3 prop4)) = if (prop1 == prop3) then Disyuncion prop1 (Conjuncion prop2 prop3)
-else (Conjuncion (Disyuncion prop1 prop2) (Disyuncion prop3 prop4))
-dis (Disyuncion (Conjuncion prop1 prop2) (Conjuncion prop3 prop4)) = if (prop1 == prop3) then Conjuncion prop1 (Disyuncion prop2 prop3)
-else (Disyuncion (Conjuncion prop1 prop2) (Conjuncion prop3 prop4))
+recorrer :: Proposicion -> [[Bool]] -> Int -> Int -> [Char]
+recorrer _ [] _  _                = ""
+recorrer prop listaBools i largoListComb =
+        let
+            variables = vars prop
+            n = length variables
+            asociacion = as_vals variables (head listaBools)
+            evaluacion_es_verdadera = evalProp asociacion prop
+            
+            --Contexto definido y evaluado
+        in
+            if not evaluacion_es_verdadera then
+                if i < largoListComb then
+                    unificarFNC variables (head listaBools) n ++ " && "  ++ recorrer prop (drop 1 listaBools) (i+1) largoListComb
+                    --Caso elemento de la lista no final
+                else
+                    unificarFNC variables (head listaBools) n  ++ recorrer prop (drop 1 listaBools) (i+1) largoListComb
+                    --Caso elemento de la lista final
+            else
+                recorrer prop (drop 1 listaBools) (i+1) largoListComb
+                --siguiente elemento
 
---IDEMPOTENCIA
-ide (Conjuncion prop1 prop2) = if (prop1 == prop2) then prop1 else Conjuncion prop1 prop2
-ide (Disyuncion prop1 prop2) = if (prop1 == prop2) then prop1 else Disyuncion prop1 prop2
-
---NEUTRO
-ne (Disyuncion prop1 (Constante prop2)) = if (Constante(prop2) == Constante(False)) then prop1 else Disyuncion prop1 (Constante prop2)
-ne (Conjuncion prop1 (Constante prop2)) = if (Constante(prop2) == Constante(True)) then prop1 else Conjuncion prop1 (Constante prop2)
---CASO ALTERNO
---DBEERÍA EVALUARLO CON
---ne (Disyuncion (Constante prop1) prop2) = if (Constante(prop1) == Constante(False)) then prop2 else Disyuncion (Constante prop1) prop2
---ne (Conjuncion (Constante prop1) prop2) = if (Constante(prop1) == Constante(True)) then prop2 else Conjuncion (Constante prop1) prop2
+--Genera la forma normal disyuntiva de una proposición
 
 
---INVERSOS
-inv (Disyuncion prop1 (Negacion prop2)) = if (prop1 == prop2) then Constante(True) else Disyuncion prop1 (Negacion prop2)
-inv (Conjuncion prop1 (Negacion prop2)) = if (prop1 == prop2) then Constante(False) else Conjuncion prop1 (Negacion prop2)
---CASO ALTERNO
---DEBERÍA EVALUARLO CON
 
---ABSORCIÓN
-abs (Disyuncion prop1 (Conjuncion prop2 prop3)) = if (prop1 == prop2) then prop1 else Disyuncion prop1 (Conjuncion prop2 prop3)
-abs (Conjuncion prop1 (Disyuncion prop2 prop3)) = if (prop1 == prop2) then prop1 else Conjuncion prop1 (Disyuncion prop2 prop3)
-
---EXPORTACIÓN
-exp (Implicacion prop1 (Implicacion prop2 prop3)) = Implicacion (Conjuncion prop1 prop2) prop3
---NOTA COLOCAR FALSE EN LOS ELSE PARA SABER QUE LA EVALUACIÓN DEBE CONTINUAR
+fnc :: Proposicion -> [Char]
+fnc prop =
+    let
+        variables = vars prop
+        n = length variables
+        lista_combinaciones_booleanas = gen_bools n
+        --Arriba se definen las variables a utilizar
+        --Recorrer recorre el total de combinaciones
+        resultado = recorrer prop lista_combinaciones_booleanas 1 (length lista_combinaciones_booleanas)
+        --Llamada a recorrer, definiendo contador en 1
+    in
+       if (resultado == "")
+           then show "No hay una FNC de la proposición dada, ya que es una Tautología"
+       else
+           resultado
